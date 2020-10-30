ce9c95e99ed5afa24c673c5773f62abd3d5b8759
Baptiste LECHAT <baptiste.lechat@ynov.com>
2020-10-30 15:02:56 +0100

    ✨ Define email is unique

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -6,10 +6,14 @@ use App\Repository\UserRepository;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Validator\Constraints as Assert;
-
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 /**
  * @ORM\Entity(repositoryClass=UserRepository::class)
+ * @UniqueEntity(
+ *  fields={"email"},
+ *  message="Votre email est déjà utilisé"
+ * )
  */
 class User implements UserInterface
 {
@@ -22,6 +26,7 @@ class User implements UserInterface
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\Email()
      */
     private $email;
 
